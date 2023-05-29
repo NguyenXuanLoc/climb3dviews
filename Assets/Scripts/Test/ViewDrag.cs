c289cbe98412ef8422e7eca1fb09453757983170
NguyenXuanLoc <59056264+NguyenXuanLoc@users.noreply.github.com>
2023-05-29 14:36:54 +0700

    Update action drag.

--- Assets/Scripts/Test/ViewDrag.cs
+++ Assets/Scripts/Test/ViewDrag.cs
@@ -1,43 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ViewDrag : MonoBehaviour
 {
-    Vector3 hit_position = Vector3.zero;
-    Vector3 current_position = Vector3.zero;
-    Vector3 camera_position = Vector3.zero;
-    float z = 0.0f;
-    // Start is called before the first frame update
-  void Update(){
-        if (Utils.isJoyStick || Utils.isRotate || Input.touchCount==2 || Utils.isTwoTouch) return;
-    if(Input.GetMouseButtonDown(0) ){ 
-        hit_position = Input.mousePosition;
-        camera_position = transform.position;
+    Vector3 offset;
+    public string destinationTag = "DropArea";
 
+    void Update()
+    {
+        if (Utils.isJoyStick || Utils.isRotate || Input.touchCount == 2 || Utils.isTwoTouch) return;
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnMouseDown();
+
+        }
+        if (Input.GetMouseButton(0))
+        {
+            OnMouseDrag();
+        }
     }
-    if(Input.GetMouseButton(0)){
-        current_position = Input.mousePosition;
-        LeftMouseDrag();        
+
+    void OnMouseDown()
+    {
+        offset = transform.position - MouseWorldPosition();
+        /*  transform.GetComponent<Collider>().enabled = false;*/
     }
-}
 
-void LeftMouseDrag(){ 
-    // From the Unity3D docs: "The z position is in world units from the camera."  In my case I'm using the y-axis as height
-    // with my camera facing back down the y-axis.  You can ignore this when the camera is orthograhic.
-    current_position.z = hit_position.z = camera_position.y;
+    void OnMouseDrag()
+    {
+        transform.position = MouseWorldPosition() + offset;
+    }
 
-    // Get direction of movement.  (Note: Don't normalize, the magnitude of change is going to be Vector3.Distance(current_position-hit_position)
-    // anyways.  
-    Vector3 direction = Camera.main.ScreenToWorldPoint(current_position) - Camera.main.ScreenToWorldPoint(hit_position);
+    void OnMouseUp()
+    {
+        var rayOrigin = Camera.main.transform.position;
+        var rayDirection = MouseWorldPosition() - Camera.main.transform.position;
+        RaycastHit hitInfo;
+        if (Physics.Raycast(rayOrigin, rayDirection, out hitInfo))
+        {
+            if (hitInfo.transform.tag == destinationTag)
+            {
+                transform.position = hitInfo.transform.position;
+            }
+        }
+        //  transform.GetComponent<Collider>().enabled = true;
+    }
 
-    // Invert direction to that terrain appears to move with the mouse.
-    direction = direction * -1.4f;
-
-    Vector3 position = camera_position + direction;
-
-    transform.position = position;
-     Utils.setX(position.x);
-     Utils.setY(position.y);
+    Vector3 MouseWorldPosition()
+    {
+        var mouseScreenPos = Input.mousePosition;
+        mouseScreenPos.z = Camera.main.WorldToScreenPoint(transform.position).z;
+        return Camera.main.ScreenToWorldPoint(mouseScreenPos);
     }
 }
